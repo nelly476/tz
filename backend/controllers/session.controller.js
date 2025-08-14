5314494ea5530fed06720298da7a658a7500180f
Nelly <nellybulatova12@email.com>
2025-08-14 18:54:09 +0300

    decrease memory use

--- backend/controllers/session.controller.js
+++ backend/controllers/session.controller.js
@@ -1,56 +1,8 @@
-// import { items } from '../data/items.js';
-
-// const sessionStore = {
-//   data: [...items],
-// };
-
-// function getPaginatedData(offset, limit, search) {
-//   if (search) {
-//     return sessionStore.data.filter(item => item.name.includes(search)).slice(offset, offset + limit)
-//   }
-
-//   return sessionStore.data
-//     .slice(offset, offset + limit)
-// }
-
-// export const getItems = (req, res) => {
-//   let {offset, limit, search} = req.query
-//   offset = Number(offset)
-//   limit = Number(limit)
-
-//   let paginated = getPaginatedData(offset, limit, search)
-
-//     res.json(paginated)
-  
-// }
-
-// export const updateSelection = (req, res) => {
-//   const {id} = req.body;
-
-//   const targetIndex = sessionStore.data.findIndex(item => item.id === id)
-//   const targeItem = sessionStore.data[targetIndex]
-//   sessionStore.data[targetIndex] = {...targeItem, isSelected: !targeItem.isSelected}
-
-//   res.json(id)
-//   }
-
-// export const updateSorting = (req, res) =>  {
-//   const {reorderedList, active, over} = req.body
-//   const fromIndex = sessionStore.data.findIndex(item => item.id === active.id)
-//   const toIndex = sessionStore.data.findIndex(item => item.id === over.id)
-
-//   const item = sessionStore.data.splice(fromIndex, 1)[0];
-//   sessionStore.data.splice(toIndex, 0, item);
-
-//   res.json(reorderedList)
-// }
-
-
 import { items } from '../data/items.js';
 import { clamp, normalizeSearch, arrayMoveInPlace } from '../../frontend/src/lib/utils.js';
 
 const sessionStore = {
-  data: items.map((it, i) => ({ order: i, ...it })),
+  data: [...items]
 };
 
 
--- backend/data/items.js
+++ backend/data/items.js
@@ -2,4 +2,5 @@ export const items = Array.from({ length: 1000000 }, (_, i) => ({
   id: i + 1,
   name: `Элемент ${i + 1}`,
   isSelected: false,
+  order: i 
 }))
